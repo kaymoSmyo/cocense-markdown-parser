ab234e484892f1397b1a6b0620d4b73a804cc858
kaymoSmyo <aymsuq@gmail.com>
2025-06-03 20:36:09 +0900

    雛型の作成 takeTextの部分をインデントが終了したら終わるようにしなくてはいけない

--- app/Main.hs
+++ app/Main.hs
@@ -1,38 +1,46 @@
 module Main (main) where
 
-import Control.Applicative
 import Data.Attoparsec.Text
+import Data.Char (isSpace)
 import Data.Text qualified as T
-
--- import Text.ParserCombinators.ReadP (skipSpaces)
 
 main :: IO ()
 main = do
     case t of
         Right str -> putStr str
-        Left str -> putStr str
+        Left err -> putStrLn $ "Parse error: " ++ err
 
 t :: Either String String
-t = parseOnly example tmp
+t = parseOnly parseCustomCodeBlock tmp
 
-example :: Parser String
-example =
-    string "code:"
-        *> ( formatCode
-                <$> takeWhile1 (/= '\n')
-                <*> (stripLeadingTabs <$> takeWhile1 (const True))
-           )
+-- | Parses the custom "code:" block format and converts it to Markdown.
+parseCustomCodeBlock :: Parser String
+parseCustomCodeBlock = do
+    _ <- string "code:"
+    -- Language name: non-space, non-newline characters
+    lang <- takeWhile1 (\c -> not (isSpace c) && c /= '\n')
+    -- Consume the rest of the header line (optional description)
+    _ <- takeTill (== '\n')
+    _ <- char '\n' -- Consume the newline character after the header
+
+    -- The rest of the input is the code block content
+    codeBlockRaw <- takeText
+
+    let processedCode = stripLeadingSpacePerLine codeBlockRaw
+    return $ formatToMarkdown lang processedCode
+
+-- | Removes a single leading space from each line of the input Text.
+stripLeadingSpacePerLine :: T.Text -> T.Text
+stripLeadingSpacePerLine inputText = T.unlines $ map processLine (T.lines inputText)
   where
-    formatCode :: T.Text -> T.Text -> String
-    formatCode lang code = T.unpack $ "```" `T.append` lang `T.append` code `T.append` "```\n"
+    processLine :: T.Text -> T.Text
+    processLine line = case T.uncons line of
+        Just (' ', rest) -> rest -- If line starts with a space, remove it
+        _ -> line -- Otherwise, keep the line as is
 
-    stripLeadingTabs :: T.Text -> T.Text
-    stripLeadingTabs text = T.unlines $ map processLine (T.lines text)
-      where
-        processLine :: T.Text -> T.Text
-        processLine line = case T.uncons line of
-            Just (_, tailText) -> tailText
-            Nothing -> T.empty
+-- | Formats the language and code into a Markdown fenced code block.
+formatToMarkdown :: T.Text -> T.Text -> String
+formatToMarkdown lang code = T.unpack $ T.concat ["```", lang, "\n", code, "```\n"]
 
 tmp :: T.Text
 tmp = "code:haskell\n tmp = 6\n tmp2 = 6\n     where\n"
