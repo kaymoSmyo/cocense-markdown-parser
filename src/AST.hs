a0621646c7b96326cbfb0d8a0f89bbec5115ae01
kaymoSmyo <atacaym@proton.me>
2026-05-31 00:28:29 +0900

    ASTの変更

--- src/AST.hs
+++ src/AST.hs
@@ -1,67 +1,73 @@
-module AST (Document (..), Block (..), Inline (..)) where
+module AST where
 
 import Data.Text (Text)
 
--- | ドキュメント全体を表すAST
-newtype Document = Document [Block]
-    deriving (Show, Eq)
+{--
+基本方針
 
--- | ブロックレベルの要素
-data Block
-    = -- | 段落 (インライン要素のリスト) 改行までの文を意味する
-      Paragraph [Inline]
-    | -- | コードブロック (言語指定と内容)
-      CodeBlock Text Text
-    | -- | リストアイテム (インデントレベルと子ブロック)
-      UListItem Word [Block]
-    | -- | 空行
-      -- 他のブロック要素 (見出し、引用など) はScrapboxの記法に合わせて追加
-      BlankLine
-    deriving (Show, Eq)
+情報が多い状態から少ない状態にはできるが、その逆は難しい
+そのため、なるべくcosenseの記法をASTに翻訳する
+その後、mdに変換するときに、mdの記法にないものをmdの記法に合わせる
 
--- | インラインレベルの要素
-data Inline
-    = -- | 装飾のないテキスト
-      PlainText Text
-    | -- | 太字 [* ] * は複数個
-      Bold Word Text
-    | -- | イタリック [/ ]
-      Italic Text
-    | -- | コードスパン ``
-      CodeSpan Text
-    | -- | リンク (リンク先と表示テキスト)
-      Link Text (Maybe Text)
-    | -- | 数式 [$ ]
-      Math Text
-    | -- | ハッシュタグ #
-      HashTag Text
-    | -- | 画像 (画像URLと表示テキスト)
-      -- 他のインライン要素 (打ち消し線、下線など) はScrapboxの記法に合わせて追加
-      Image Text (Maybe Text)
-    | -- | []記法
-      RefText Text
-    deriving (Show, Eq)
+--}
+{--
+ブロック要素は複数行をもつ
 
-example1 :: Document
-example1 =
-    Document
-        [ UListItem
-            1
-            [ Paragraph
-                [ PlainText "aiue"
-                , Link "aiue" Nothing -- [aiue]
-                , CodeSpan "code" -- `code`
-                , Bold 1 "aiue" -- [* aiue]
-                ]
-            ]
+一行はインライン複数のインライン要素の集まり
+    行の種類
+        コマンドライン
+        引用
+        プレーン
+インライン要素は[]の中で表記される
+    インライン要素
+        リンク
+            Enhance (Link "aiu")([- [aiu]])はcosenseでok
+            Link (Enhance "aiu")([[- aiu]])は"[Enhance "aiu"]"になる ただ[]で囲まれているだけ
+        [ 強調
+        , 斜体
+        , 打消し
         ]
+            これらは順番を入れ替えても結果は同じ
+            修飾の一つ以上の組合せと考える
+        二重大括弧での強調
+        URL
+            前 or 後ろに名前を付けることも可能
+            Enhanceでリンクの文字の加工も可能
+        ハッシュタグ
+            Enhanceで修飾可能
+        Tex
+        コード
 
--- 一つインデントされ、リストアイテムになったコードブロック
-example2 :: Document
-example2 =
-    Document
-        [ UListItem
-            1
-            [ CodeBlock "Haskell" "tmp = 0\ntmp2 = 0"
-            ]
-        ]
+--}
+
+data Enhace
+    = Bold Word
+    | Italic
+    | CrossOut
+    deriving (Show, Eq)
+data InlineElem
+    = Enhaced [Enhace] InlineElem
+    | Plain Text
+    | Link Text
+    | HashTag Text
+    | URL Text (Maybe Text)
+    | Tex Text
+    | Code Text
+    deriving (Show, Eq)
+
+-- -- | ドキュメント全体を表すAST
+-- newtype Document = Document [Block]
+--     deriving (Show, Eq)
+
+-- -- | ブロックレベルの要素
+-- data Block
+--     = -- | 段落 (インライン要素のリスト) 改行までの文を意味する
+--       Paragraph [Inline]
+--     | -- | コードブロック (言語指定と内容)
+--       CodeBlock Text Text
+--     | -- | リストアイテム (インデントレベルと子ブロック)
+--       UListItem Word [Block]
+--     | -- | 空行
+--       -- 他のブロック要素 (見出し、引用など) はScrapboxの記法に合わせて追加
+--       BlankLine
+--     deriving (Show, Eq)
