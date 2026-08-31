4f92cd404923f738abef27e138650c75edb407bd
kaymoSmyo <atacaym@proton.me>
2026-08-31 18:14:53 +0900

    自作テンプレートに変更

--- flake.nix
+++ flake.nix
@@ -1,43 +1,25 @@
 {
-  description = "Example Haskell development environment for Zero to Nix";
+  description = "Haskell project template GHC910";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/566acc0";
   };
 
   outputs =
     { self, nixpkgs }:
     let
-      allSystems = [
-        "x86_64-linux" # 64-bit Intel/AMD Linux
-      ];
-
-      forAllSystems =
-        f:
-        nixpkgs.lib.genAttrs allSystems (
-          system:
-          f {
-            pkgs = import nixpkgs { inherit system; };
-          }
-        );
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      hpkgs = pkgs.haskell.packages.ghc910;
     in
     {
-      # Development environment output
-      devShells = forAllSystems (
-        { pkgs }:
-        let
-          hpkgs = pkgs.haskell.packages.ghc910;
-        in
-        {
-          default = pkgs.mkShell {
-            packages = [
-              hpkgs.cabal
-              hpkgs.ghc
-              hpkgs.haskell-language-server
-              hpkgs.cabal-fmt
-            ];
-          };
-        }
-      );
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [
+          hpkgs.cabal-install
+          hpkgs.ghc
+          hpkgs.haskell-language-server
+          hpkgs.cabal-fmt
+        ];
+      };
     };
 }
